2781ed4947e41f52d2de4034e65e206ecc587932
Robert Lupas <robertlupas09@gmail.com>
2026-03-30 12:52:49 +0300

    Only show log title if there are logs

--- index.ts
+++ index.ts
@@ -34,6 +34,7 @@ if (screen.availWidth < 800 || screen.availHeight < 700) {
     }
 
     function tryAndDisplayError(func: () => void) {
+        clearLog();
         try {
             func();
         } catch (e) {
@@ -43,11 +44,13 @@ if (screen.availWidth < 800 || screen.availHeight < 700) {
 
     function displayLog(log: string, error: Boolean = false) {
         clearLog();
+        (document.getElementById("logsTitle") as HTMLSpanElement).style.display = "block";
         if (error) (document.getElementById("errors") as HTMLSpanElement).textContent = log;
         else (document.getElementById("log") as HTMLSpanElement).textContent = log;
     }
 
     function clearLog() {
+        (document.getElementById("logsTitle") as HTMLSpanElement).style.display = "none";
         (document.getElementById("log") as HTMLSpanElement).textContent = "";
         (document.getElementById("errors") as HTMLSpanElement).textContent = "";
     }
