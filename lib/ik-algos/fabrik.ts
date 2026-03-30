326a7e81919cfe7a7fbbeae869933e30ebb1d25c
Robert Lupas <robertlupas09@gmail.com>
2026-03-30 22:10:58 +0300

    Update fabrik.ts

--- lib/ik-algos/fabrik.ts
+++ lib/ik-algos/fabrik.ts
@@ -1,13 +1,14 @@
 import { type Bone, type Point, totalLength, distance } from "../math";
 
 export function fabrik(base: Point, target: Point, bones: Bone[], tolerance: number = 0.1): Bone[] {
-    if (totalLength(bones) < Math.sqrt((target.x - base.x) ** 2 + (target.y - base.y) ** 2))
+    if (totalLength(bones) < distance(base, target))
         throw new Error("Target unreachable");
 
+    let result: Bone[] = bones.map(b => ({ ...b }));
     let points: Point[] = [];
     let currentPoint = { x: base.x, y: base.y };
     points.push(currentPoint);
-    for (let bone of bones) {
+    for (let bone of result) {
         currentPoint = {
             x: currentPoint.x + bone.length * Math.cos(bone.angle),
             y: currentPoint.y + bone.length * Math.sin(bone.angle)
@@ -15,16 +16,15 @@ export function fabrik(base: Point, target: Point, bones: Bone[], tolerance: num
         points.push(currentPoint);
     }
 
-    let targetDistance = distance(points[points.length - 1]!, target);
-
-    while (targetDistance > tolerance) {
+    for (let iter = 0; iter < 100; iter++) {
+        if (distance(points.at(-1)!, target) <= tolerance) break;
         // Backward reaching
         points[points.length - 1] = { x: target.x, y: target.y };
         for (let i = points.length - 2; i >= 0; i--) {
             let dx = points[i]!.x - points[i + 1]!.x;
             let dy = points[i]!.y - points[i + 1]!.y;
             let dist = distance(points[i]!, points[i + 1]!);
-            let ratio = bones[i]!.length / dist;
+            let ratio = result[i]!.length / dist;
             points[i] = {
                 x: points[i + 1]!.x + dx * ratio,
                 y: points[i + 1]!.y + dy * ratio
@@ -37,22 +37,20 @@ export function fabrik(base: Point, target: Point, bones: Bone[], tolerance: num
             let dx = points[i + 1]!.x - points[i]!.x;
             let dy = points[i + 1]!.y - points[i]!.y;
             let dist = distance(points[i + 1]!, points[i]!);
-            let ratio = bones[i]!.length / dist;
+            let ratio = result[i]!.length / dist;
             points[i + 1] = {
                 x: points[i]!.x + dx * ratio,
                 y: points[i]!.y + dy * ratio
             };
         }
-
-        targetDistance = distance(points[points.length - 1]!, target);
     }
 
     // Update bone angles
-    for (let i = 0; i < bones.length; i++) {
+    for (let i = 0; i < result.length; i++) {
         let dx = points[i + 1]!.x - points[i]!.x;
         let dy = points[i + 1]!.y - points[i]!.y;
-        bones[i]!.angle = Math.atan2(dy, dx);
+        result[i]!.angle = Math.atan2(dy, dx);
     }
 
-    return bones;
+    return result;
 }
